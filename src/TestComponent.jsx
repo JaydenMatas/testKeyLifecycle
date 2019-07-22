16f098117e50d81ee0f0d17a4d973d7f594d0d86
Jayden Matas <jayden.matas@gmail.com>
2019-07-22 20:46:47 +1000

    test case of unique key and rendering children on parent state change

---
+++ src/TestComponent.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import shortid from 'shortid';
+import TestComponent2 from './TestComponent2'
+class TestComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.compKey = shortid.generate();
+        this.state = {
+            valueToCauseReRender: 're-render-1'
+        }
+    }
+
+    componentDidMount() {
+        this.compKey = shortid.generate();
+        // this.setState({valueToCauseReRender: 're-render-2'})
+    }
+
+    render() {
+        return <div key={this.compKey}><TestComponent2 /></div>
+    }
+}
+
+export default TestComponent
---
+++ src/TestComponent2.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import shortid from 'shortid';
+
+class TestComponent2 extends React.Component {
+
+    render() {
+        return <div> test component 2 content goes here</div>
+    }
+}
+
+export default TestComponent2
